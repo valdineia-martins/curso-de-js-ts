0ef6f8b2a5b60377a5ec86abeb49ff6a28116cc7
valdineia-martins <valdineia.dev@gmail.com>
2024-01-31 17:14:17 -0300

    exercicio de converter string em number

---
+++ js/aula2.js
@@ -0,0 +1,9 @@
+let num1 = prompt("Digite um número");
+let num2 = prompt("Digite outro número");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+let resultado = (num1 + num2);
+
+alert('O resultado foi: '  +   resultado);
